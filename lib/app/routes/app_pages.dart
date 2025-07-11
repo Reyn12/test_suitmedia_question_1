41bc066dd1f61cd7c4e54806b41fcb561b90c19e
Reyn12 <muhammadrenaldi687@gmail.com>
2025-07-11 17:06:08 +0700

    feat: create three new screens (First, Second, Third) using GetX and add their routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,13 @@
 import 'package:get/get.dart';
 
+import '../modules/first_screen/bindings/first_screen_binding.dart';
+import '../modules/first_screen/views/first_screen_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/second_screen/bindings/second_screen_binding.dart';
+import '../modules/second_screen/views/second_screen_view.dart';
+import '../modules/third_screen/bindings/third_screen_binding.dart';
+import '../modules/third_screen/views/third_screen_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +21,21 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.FIRST_SCREEN,
+      page: () => const FirstScreenView(),
+      binding: FirstScreenBinding(),
+    ),
+    GetPage(
+      name: _Paths.SECOND_SCREEN,
+      page: () => const SecondScreenView(),
+      binding: SecondScreenBinding(),
+    ),
+    GetPage(
+      name: _Paths.THIRD_SCREEN,
+      page: () => const ThirdScreenView(),
+      binding: ThirdScreenBinding(),
     ),
   ];
 }
